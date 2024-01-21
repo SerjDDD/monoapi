c398e29b644936dcfece7d13096f8434782cd68a
Sergii Aleksieiev <sergii.aleksieiev@fiverr.com>
2024-01-21 12:35:11 +0200

    fixed port handling

--- index.js
+++ index.js
@@ -59,6 +59,8 @@ const handler = async (req, res) => {
 app.get('*', handler);
 app.post('*', handler);
 
-app.listen(80, () => {
-    console.log('Server is running on port 3000');
+const port = process.env.PORT || 3000;,
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
